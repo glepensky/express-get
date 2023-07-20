4cf4849e3b701b85201d2208aa471f4d4aebf579
Gabrielle Lepensky <gabrielle.lepensky@gmail.com>
2023-07-19 20:48:15 -0500

    fixed

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,26 @@
 console.log('script sourced.');
+
+function getQuotes(){
+    axios.get('/quotes').then((response) => {
+        console.log("success", response.data);
+        let quotesFromServer = response.data
+        rendertoDom(quote)
+// good indication of end of route })
+}).catch((error) => {
+    console.log(error);
+    alert("something went wrong")
+})
+}
+
+getQuotes()
+
+function rendertoDom(quotes){
+    let outputList = document.querySelector('#output')
+    outputList.innerHTML = ''
+
+    for(let quote of quotes){
+        outputList.innerHTML += `
+            <p>${quote.text} - ${quote.author} </p>
+        `
+    }
+}
